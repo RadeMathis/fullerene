c87735cab50f4d61ce52f42beb8355d8a78b68b3
RadeMathis <rade.mathis@free.fr>
2015-07-16 13:07:32 +0200

    New "replier*(...)" seems to work

--- src/test.cpp
+++ src/test.cpp
@@ -15,12 +15,20 @@ main(void)
     vector<int> v;
     g1->initialiserPenta();
     Graph* g2 = g1->replierPenta(6,3);
-    g1->writeInFile("cetExemple");
-    g2->writeInFile("detExemple");
+    Graph* g3 = g2-> replierPenta(7,0);
+    Graph* g4 = g2-> replierPenta(7,3);
+    if(g1)
+        g1->writeInFile("cetExemple");
+    if(g2)
+        g2->writeInFile("detExemple");
+    if(g3)
+        g3->writeInFile("eetExemple");
+    if(g4)
+        g4->writeInFile("fetExemple");   
     graphPrinter("cetExemple");
     graphPrinter("detExemple");
-
-
+    graphPrinter("eetExemple");
+    graphPrinter("detExemple");
 
     delete g1;
     delete g2;
