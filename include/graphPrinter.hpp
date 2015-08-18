75dd35df039689dd53358b18c1576e863278b094
RadeMathis <rade.mathis@free.fr>
2015-08-18 19:52:55 +0200

    Adding graphPrinter_all to print all in graphList.data

--- include/graphPrinter.hpp
+++ include/graphPrinter.hpp
@@ -2,7 +2,7 @@
  * Created by Rade Mathis for LaBRI
  * All right reserved by the LaBRI
  * * *
- * Version 0.1 ( 20/06/2015 )
+ * Version 0.2 ( 11/08/2015 )
  * * *
  * Supposed to transform a .graph.data into .graph.py
  * * */
@@ -12,14 +12,26 @@
 #include <string>
 
 void graphPrinter(std::string, std::string dir);
-	/* This function takes a .graph.data file (call it without .graph.data), 
-	 * translate it into a .graph.py Using the command : 
-	 * python <myFile>.graph will print the graph in your default browser.
-	 * Will put .gaph.py in dir
-	 # Cette fonction prend comme paramettre un fichier .graph.data et le
-	 # traduit en .graph.py (passer seulement <monFichier>.graph en parametre)
-	 # La commande : pyhton <monFichier>.graph affichera une image du graphe
-	 # dans le navigateur par defaut. */
+    /* This function takes a .graph.data file (call it without .graph.data), 
+     * translate it into a .graph.py Using the command : 
+     * python <myFile>.graph will print the graph in your default browser.
+     * Will put .gaph.py in dir
+     # Cette fonction prend comme paramettre un fichier .graph.data et le
+     # traduit en .graph.py (passer seulement <monFichier>.graph en parametre)
+     # La commande : pyhton <monFichier>.graph affichera une image du graphe
+     # dans le navigateur par defaut. */
 void graphPrinter(std::string);
-	/* Just call graphPrinter( , "");
-	 # Appelle juste graphPrinter( , ""); */
+    /* Just call graphPrinter( , "");
+     # Appelle juste graphPrinter( , ""); */
+
+void graphPrinter_all(std::string outPath, std::string inPath);
+    /* For all the graphs in inPath/graphLists.data will call :
+     * graphPrinter(inPath/<name of file>, outPath);
+     # Tout les graphes recenses dans inPath/graphList.data seront
+     # traduits par graphPrinter(inPath/<nom du fichier>, outPath); */
+void graphPrinter_all(std::string outPath);
+    /* Just call graphPrinter_all(outPath, ".");
+     # Appelle juste graphPrinter_all(outPath, "."); */
+void graphPrinter_all();
+    /* Just call graphPrinter_all(".");
+     # Appelle juste graphPrinter_all("."); */
